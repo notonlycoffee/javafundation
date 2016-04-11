bdec377c364a796178736a7fd18c6fc9ebecc36d
q <q@Lenovo-PC>
2016-04-11 19:01:07 +0800

    补充io2

--- src/main/java/com/audaque/wwq/wwq/core/io/IODemo.java
+++ src/main/java/com/audaque/wwq/wwq/core/io/IODemo.java
@@ -1,14 +1,17 @@
 package com.audaque.wwq.wwq.core.io;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.RandomAccessFile;
 import java.io.Reader;
 import java.io.Writer;
@@ -86,6 +89,8 @@ public class IODemo {
 	}
 	
 	
+	
+	//输出所有的文件名字,如果是目录,则一直遍历输入这个目录里面所有的文件,
 	@Test
 	public void demo6() {
 		File file = new File("D:"+File.separator+"医疗数据");
@@ -153,7 +158,7 @@ public class IODemo {
 	
 	
 	
-	//想文件中写入字符串,将整个字符串直接写入文件
+	//向文件中写入字符串,将整个字符串直接写入文件
 	@Test
 	public void demo9() {
 		File file = new File("D:"+File.separator+"one.txt");
@@ -174,7 +179,7 @@ public class IODemo {
 	}
 	
 	
-	//想文件中写入字符串,将整个字符串一个字节一个字节地写入文件中
+	//向文件中写入字符串,将整个字符串一个字节一个字节地写入文件中
 	@Test
 	public void demo10() {
 		File file = new File("D:"+File.separator+"one.txt");
@@ -214,7 +219,7 @@ public class IODemo {
 		File file = new File("D:"+File.separator+"one.txt");
 		OutputStream out = null;
 		try {
-			out = new FileOutputStream(file,true);
+			out = new FileOutputStream(file,true);//设置文件后面追加内容
 			out.write("\nhelo".getBytes());
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -389,22 +394,226 @@ public class IODemo {
 		File source = new File("D:"+File.separator+"one.txt");
 		File dest = new File("D:"+File.separator+"two.txt");
 		
+		InputStream in = null;
+		OutputStream out = null;
+		
 		if(!source.exists()) {
 			System.out.println("文件不存在");
 		}
 		if(!dest.exists()) {
 			try {
 				dest.createNewFile();
+				System.out.println("目标文件创建成功");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+		int len = 0 ;
+		int temp = 0;
+		
+		try {
+			in = new FileInputStream(source);
+			out = new FileOutputStream(dest);
+			
+			while((temp=in.read())!=-1) {
+				out.write(temp);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				in.close();
+				out.close();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		
-		
-		
+	}
+	
+	
+	//将文件的字节输出流转换为字符输出流
+	@Test
+	public void demo20() {
+		File file = new File("D:"+File.separator+"one.txt");
+		Writer out = null;
+		try {
+			out = new OutputStreamWriter(new FileOutputStream(file));
+			out.write("你知道");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//将文件的字节输入流转换成字符输入流
+	@Test
+	public void demo21() {
+		File file = new File("D:"+File.separator+"one.txt");
+		Reader in = null;
+		try {
+			in = new InputStreamReader(new FileInputStream(file));
+			char c [] = new char[100];
+			int len = in.read(c);
+			System.out.println(new String(c,0,len));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	//使用内存操作流,内容中存在字符串"what a fuck day",将其读入到内存的内存输入流中,
+	//然后从内存输入流写出到内存输出流,直接将内存输出流的内容打印出来
+	@Test
+	public void demo22() {
+		ByteArrayInputStream in = null;
+		ByteArrayOutputStream out = null;
+		String value = "what a fuck day";
+		in = new ByteArrayInputStream(value.getBytes());
+		out = new ByteArrayOutputStream();
+		int len = 0;
+		while((len=in.read())!=-1) {
+			char c = (char) len;
+			out.write(Character.toUpperCase(c));
+		}
+		System.out.println(out.toString());
+		try {
+			in.close();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
+	
+	
+	//跟上面demo22的例子一样的,只是在读取和写出的时候,使用固定大小的变量进行读取
+	@Test
+	public void demo23() {
+		ByteArrayInputStream in = null;
+		ByteArrayOutputStream out = null;
+		String value = "what a fuck dayhat a fuck dayhat a fuck dayhat a fuck dayhat a fuck day";
+		in = new ByteArrayInputStream(value.getBytes());
+		out = new ByteArrayOutputStream();
+		int len = 0;
+		byte [] b = new byte [10];
+		try {
+			len = in.read(b);
+			out.write(b);
+			System.out.println(new String(b,0,len).toUpperCase());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		
+		try {
+			in.close();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	//管道流
+	//这里暂时不提供例子,或者直接拷贝网站例子
+	@Test
+	public void demo24() {
+//		管道流主要可以进行两个线程之间的通信。
+//
+//		PipedOutputStream 管道输出流
+//
+//		PipedInputStream 管道输入流
+//
+//		验证管道流
+//
+//		/**
+//		 * 验证管道流
+//		 * */
+//		import java.io.*;
+//		 
+//		/**
+//		 * 消息发送类
+//		 * */
+//		class Send implements Runnable{
+//		 private PipedOutputStream out=null;
+//		 public Send() {
+//		 out=new PipedOutputStream();
+//		 }
+//		 public PipedOutputStream getOut(){
+//		 return this.out;
+//		 }
+//		 public void run(){
+//		 String message="hello , Rollen";
+//		 try{
+//		 out.write(message.getBytes());
+//		 }catch (Exception e) {
+//		 e.printStackTrace();
+//		 }try{
+//		 out.close();
+//		 }catch (Exception e) {
+//		 e.printStackTrace();
+//		 }
+//		 }
+//		}
+//		 
+//		/**
+//		 * 接受消息类
+//		 * */
+//		class Recive implements Runnable{
+//		 private PipedInputStream input=null;
+//		 public Recive(){
+//		 this.input=new PipedInputStream();
+//		 }
+//		 public PipedInputStream getInput(){
+//		 return this.input;
+//		 }
+//		 public void run(){
+//		 byte[] b=new byte[1000];
+//		 int len=0;
+//		 try{
+//		 len=this.input.read(b);
+//		 }catch (Exception e) {
+//		 e.printStackTrace();
+//		 }try{
+//		 input.close();
+//		 }catch (Exception e) {
+//		 e.printStackTrace();
+//		 }
+//		 System.out.println("接受的内容为 "+(new String(b,0,len)));
+//		 }
+//		}
+//		/**
+//		 * 测试类
+//		 * */
+//		class hello{
+//		 public static void main(String[] args) throws IOException {
+//		 Send send=new Send();
+//		 Recive recive=new Recive();
+//		 try{
+//		//管道连接
+//		 send.getOut().connect(recive.getInput());
+//		 }catch (Exception e) {
+//		 e.printStackTrace();
+//		 }
+//		 new Thread(send).start();
+//		 new Thread(recive).start();
+//		 }
+//		}
+	}
+	
+	
+	
+	
+	
 	
 	
 	
