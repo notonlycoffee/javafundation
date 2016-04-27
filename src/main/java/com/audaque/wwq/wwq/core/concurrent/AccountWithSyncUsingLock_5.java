51c61062769808213c79e09e953c4a73da32e842
jc <jc@jc-PC>
2016-04-27 23:05:18 +0800

    concurrent 补充

---
+++ src/main/java/com/audaque/wwq/wwq/core/concurrent/AccountWithSyncUsingLock_5.java
@@ -0,0 +1,50 @@
+package com.audaque.wwq.wwq.core.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AccountWithSyncUsingLock_5 {
+	private static Account account = new Account();
+	public static void main(String[] args) {
+		ExecutorService executor = Executors.newCachedThreadPool();
+		for( int i = 0 ; i < 100 ; i++) {
+			executor.execute(new AddAPennyTask());
+		}
+		executor.shutdown();
+		
+		while(!executor.isTerminated()) {
+			System.out.println(account.getBalance());
+		}
+	}
+	
+	
+	
+	private static class AddAPennyTask implements Runnable {
+		public void run() {
+			account.deposit(1);
+		}
+	}
+	
+	private static class Account {
+		private int balance;
+		private static Lock lock = new ReentrantLock(); //显式添加锁，对方法进行锁定，实现线程安全
+		public int getBalance() {
+			return this.balance;
+		}
+		
+		public void deposit(int amount) {
+			lock.lock();
+			try {
+				int temp = this.balance + amount;
+				Thread.sleep(5);
+				this.balance = temp;
+			}catch(Exception e) {
+				throw new RuntimeException(e);
+			} finally {
+				lock.unlock();
+			}
+		}
+	}
+}
--- src/main/java/com/audaque/wwq/wwq/core/concurrent/AccountWithoutSync_4.java
+++ src/main/java/com/audaque/wwq/wwq/core/concurrent/AccountWithoutSync_4.java
@@ -3,6 +3,12 @@ package com.audaque.wwq.wwq.core.concurrent;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * 调用一个对象的同步实例方法要求给该对象加锁
+ * 调用一个类的同步静态方法要求对该类加锁
+ * @author jc
+ *
+ */
 public class AccountWithoutSync_4 {
 	private static Account account = new Account();
 	public static void main(String[] args) {
@@ -13,15 +19,18 @@ public class AccountWithoutSync_4 {
 		
 		executorService.shutdown();
 		
-		while(executorService.isTerminated()) {
+		while(!executorService.isTerminated()) {
 			System.out.println(account.getBalance());
 		}
 	}
 	
 	private static class AddAccount implements Runnable {
-
 		public void run() {
-			account.deposit(1);
+				account.deposit(1);
+			/*第二种实现线程安全的方式，在任务中调用这个方法的时候使用sysnchronized
+			 * synchronized (account) {
+				account.deposit(1);
+			}*/
 		}
 		
 	}
@@ -31,6 +40,16 @@ public class AccountWithoutSync_4 {
 		public int getBalance() {
 			return this.balance;
 		}
+		/*第一种线程安全的方式，在方法签名中加入sysnchronized
+		 * public synchronized void deposit(int b) {
+			int re = this.balance + b;
+			try {
+				Thread.sleep(2);
+			} catch(Exception e) {
+				throw new RuntimeException(e);
+			}
+			this.balance = re;
+		}*/
 		public void deposit(int b) {
 			int re = this.balance + b;
 			try {
@@ -39,6 +58,16 @@ public class AccountWithoutSync_4 {
 				throw new RuntimeException(e);
 			}
 			this.balance = re;
+			/*第三种实现多线程安全的方式，在方法体中加入sysnchronized
+			 * synchronized (this) {
+				int re = this.balance + b;
+				try {
+					Thread.sleep(2);
+				} catch(Exception e) {
+					throw new RuntimeException(e);
+				}
+				this.balance = re;
+			}*/
 		}
 	}
 }
