ead6e88d02be3470293ec1a7c3c1dc54aab50f6c
q <q@192.168.56.1>
2016-04-28 18:18:22 +0800

    concurrent

--- src/main/java/com/audaque/wwq/wwq/core/concurrent/AccountWithSyncUsingLock_5.java
+++ src/main/java/com/audaque/wwq/wwq/core/concurrent/AccountWithSyncUsingLock_5.java
@@ -5,6 +5,13 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+
+/**
+ * 显式添加锁实现多线程安全问题
+ * 添加锁之后使用finally进行释放锁
+ * @author q
+ *
+ */
 public class AccountWithSyncUsingLock_5 {
 	private static Account account = new Account();
 	public static void main(String[] args) {
