4f565f0fa031c880abb3abe90f83f557fd025265
q <q@192.168.56.1>
2016-05-13 15:24:45 +0800

    f

---
+++ src/main/java/com/audaque/wwq/wwq/core/another_concurrentbook/chapter3/Main_1.java
@@ -0,0 +1,39 @@
+package com.audaque.wwq.wwq.core.another_concurrentbook.chapter3;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main_1 implements Runnable {
+	private SynchronizedCounter s;
+	public Main_1(SynchronizedCounter s) {
+		this.s = s;
+	}
+	public static void main(String[] args) {
+		SynchronizedCounter ss = new SynchronizedCounter();
+		Thread t1 = new Thread(new Main_1(ss));
+		Thread t2 = new Thread(new Main_1(ss));
+		Thread t3 = new Thread(new Main_1(ss));
+		t1.start();
+		t2.start();
+		t3.start();
+		try {
+			t1.join();
+			t2.join();
+			t3.join();
+		} catch(InterruptedException e) {
+			
+		}
+		System.out.println(ss.getNumber());
+	}
+	
+	public void run() {
+		try {
+		for(int  i = 0 ; i < 10 ; i++) {
+			s.increment();
+			System.out.println(Thread.currentThread().getName()+" : " + s.getNumber());
+			TimeUnit.SECONDS.sleep(1);
+		}
+		} catch(Exception e) {
+			
+		}
+	}
+}
---
+++ src/main/java/com/audaque/wwq/wwq/core/another_concurrentbook/chapter3/SynchronizedCounter.java
@@ -0,0 +1,18 @@
+package com.audaque.wwq.wwq.core.another_concurrentbook.chapter3;
+
+public class SynchronizedCounter{
+	private int number=0;
+	
+	public synchronized int getNumber() {
+		return this.number;
+	}
+	
+	public synchronized void increment() {
+		++this.number;
+	}
+	
+	public synchronized void decrement() {
+		--this.number;
+	}
+	
+}
